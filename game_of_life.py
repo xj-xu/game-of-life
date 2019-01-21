ee729ce43ed425d25c54f89c49339813b5be961a
XJ <33842377+xj-xu@users.noreply.github.com>
2019-01-21 10:27:28 -0600

    Add files via upload

---
+++ game_of_life.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+
+def iterate(X):
+    X1 = np.zeros((N, N))
+    X1[:,:]=X[:,:]    
+
+    for i in range(N):
+        for j in range(N):
+            nb=0
+            for k in range(8):
+                ik=i+nx[k]
+                jk=j+ny[k]
+                nb = nb+ X[ik%N,jk%N]
+            if X[i,j]==red:
+                if (nb < 2) or (nb>3): 
+                    X1[i,j]=blue
+            else:
+                if nb==3: X1[i,j]=red
+                    
+
+            
+    return X1
+        
+        
+        
+# neighbor pixel directions
+nx = [-1, -1, 0, 1, 1, 1, 0, -1]
+ny = [0, 1, 1, 1, 0, -1, -1, -1]
+blue=0
+red=1
+N = 100
+X = np.zeros((N, N),dtype=int)
+for i in range(N):
+    for j in range(N):
+        if np.random.random()>0.8 : X[i,j]=red
+
+
+
+
+
+#
+# do not change below
+#
+pause = False
+
+def onClick(event):
+    global pause
+    pause ^= True
+
+
+
+fig = plt.figure(figsize=(25/3, 6.25))
+ax = fig.add_subplot(111)
+ax.set_axis_off()
+im = ax.imshow(X, cmap=plt.cm.jet)
+
+# The animation function: called to produce a frame for each generation.
+def animate(i):
+    im.set_data(animate.X)
+    animate.X = iterate(animate.X)
+# Bind our grid to the identifier X in the animate function's namespace.
+animate.X = X
+
+# Interval between frames (ms).
+interval = 100
+fig.canvas.mpl_connect('button_press_event', onClick)
+anim = animation.FuncAnimation(fig, animate, interval=interval)
+plt.show()
